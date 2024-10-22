ab9f1757941e677dfba7e675163034eadbf84d91
Brijesh Varsani <bvarsani@aiut.com.pl>
2024-10-22 14:26:37 +0200

    pagination, max size 512x512, 10 images per page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, request
 from azure.storage.blob import BlobServiceClient
 import os
 from dotenv import load_dotenv
+from math import ceil
 
 app = Flask(__name__)
 
@@ -24,15 +25,39 @@ except Exception as e:
     container_client = blob_service_client.create_container(CONTAINER_NAME)
 
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def index():
     # List all blobs in the container
     blobs = [blob.name for blob in container_client.list_blobs()]
+    
+    # Pagination logic (10 images per page)
+    page = int(request.args.get('page', 1))
+    per_page = 10
+    total_blobs = len(blobs)
+    start = (page - 1) * per_page
+    end = start + per_page
+
+    # Get blobs for the current page
+    paginated_blobs = blobs[start:end]
+
+    # Generate the full URL for each blob in the current page
     blob_urls = [
         f"https://{blob_service_client.account_name}.blob.core.windows.net/{CONTAINER_NAME}/{blob}"
-        for blob in blobs
+        for blob in paginated_blobs
     ]
-    return render_template("index.html", blobs=blob_urls)
+
+    # Check if there are more pages
+    has_next = end < total_blobs
+    has_prev = start > 0
+
+    return render_template(
+        "index.html",
+        blobs=blob_urls,
+        current_page=page,
+        has_next=has_next,
+        has_prev=has_prev
+    )
 
 
 # Route to handle the image upload
