21fa4219d4754121816ee966da83786c72d49aee
Brijesh Varsani <bvarsani@aiut.com.pl>
2024-10-23 23:32:42 +0200

    update UI

--- static/script.js
+++ static/script.js
@@ -2,4 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
    
     document.getElementById('user_agent').value = navigator.userAgent;
     console.log("user info present");
+
+    const fileInput = document.getElementById('file-upload');
+    const fileUploadText = document.getElementById('file-upload-text');
+
+    fileInput.addEventListener('change', function() {
+        const files = fileInput.files;
+        if (files.length > 0) {
+            fileUploadText.textContent = `${files.length} image(s) selected`;
+        } else {
+            fileUploadText.textContent = 'No images selected';
+        }
+    });
 });
